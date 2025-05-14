cdd88b33bab14d792826b815d6cc8ddb659428fd
Youssef <youssefyaser2020@gmail.com>
2025-05-14 04:22:21 +0300

    Enhance Redux store with loan management functionality and add example dispatches for account actions

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -12,11 +14,30 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case 'account/requestLoan':
       if (state.loan > 0) return;
-      // LATER
-      return { ...state, loan: action.payload.loan, loanPurpose: action.payload.purpose };
+      return {
+        ...state,
+        balance: state.balance + action.payload.amount,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
     case 'account/payLoan':
       return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan };
     default:
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+// without action creators --> and thats okay
+store.dispatch({ type: 'account/deposit', payload: 500 });
+console.log(store.getState());
+
+store.dispatch({ type: 'account/withdraw', payload: 300 });
+console.log(store.getState());
+
+store.dispatch({ type: 'account/requestLoan', payload: { amount: 1000, purpose: 'Buy a car' } });
+console.log(store.getState());
+
+store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState());
