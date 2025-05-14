9b204811c50858e3d5369730d576369850051b54
Youssef <youssefyaser2020@gmail.com>
2025-05-14 14:29:43 +0300

    Implement deposit functionality in AccountOperations component, utilizing Redux for state management and enhancing user interaction with account deposits.

--- src/features/accounts/AccountOperations.jsx
+++ src/features/accounts/AccountOperations.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deposit } from './accountSlice';
 
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState('');
@@ -6,8 +8,17 @@ function AccountOperations() {
   const [loanAmount, setLoanAmount] = useState('');
   const [loanPurpose, setLoanPurpose] = useState('');
   const [currency, setCurrency] = useState('USD');
+  const dispatch = useDispatch();
 
-  function handleDeposit() {}
+  const account = useSelector((store) => store.account);
+  console.log(account);
+
+  function handleDeposit() {
+    if (!depositAmount) return;
+
+    dispatch(deposit(depositAmount));
+    setDepositAmount('');
+  }
 
   function handleWithdrawal() {}
 
