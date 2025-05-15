4c8891c49c977810889c0db54a1276db64a4af0e
Youssef <youssefyaser2020@gmail.com>
2025-05-15 15:12:57 +0300

    Add Customer component styles and update JSX for enhanced layout and user experience.

--- src/features/customers/Customer.jsx
+++ src/features/customers/Customer.jsx
@@ -1,9 +1,14 @@
 import { useSelector } from 'react-redux';
+import './Customer.css';
 
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
 
-  return <h2>👋 Welcome, {customer}</h2>;
+  return (
+    <h2 className='customer-welcome'>
+      <span>👋</span> Welcome, {customer}
+    </h2>
+  );
 }
 
 export default Customer;
