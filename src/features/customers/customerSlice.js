a65aae84bce5cae10f8ebcc86f5d198c07fb0b1d
Youssef <youssefyaser2020@gmail.com>
2025-05-15 14:53:13 +0300

    Refactor customer state management by implementing createSlice from @reduxjs/toolkit, enhancing reducer functionality for customer creation and name updates.

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,10 +1,39 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   fullName: '',
   nationalId: '',
   createdAt: '',
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return {
+          payload: { fullName, nationalId},
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = new Date().toISOString();
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export default customerSlice.reducer;
+export const { createCustomer, updateName } = customerSlice.actions;
+
+// OLD syntax of using REDUX
+
+/* export default function customerReducer(state = initialState, action) {
   switch (action.type) {
     case 'customer/createCustomer':
       return {
@@ -27,4 +56,4 @@ export function createCustomer(fullName, nationalId) {
 
 export function updateName(fullName) {
   return { type: 'customer/updateName', payload: fullName };
-}
+} */
